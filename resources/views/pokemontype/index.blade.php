e6faad94a1a7e7b089e8083cc683f7e5b3a7bed1
liuyunbo <liuyunbo@heartsone.net>
2018-06-12 10:17:08 +0800

    change style for attribute
    add margin:auto for ul

--- resources/views/pokemontype/index.blade.php
+++ resources/views/pokemontype/index.blade.php
@@ -7,8 +7,9 @@
 height:30px; line-height:30px;
 border-top:0px; font-size:12px;}
 
+    .type ul{margin: auto;}
     .type ul li{ display:block; width:5.2%; float:left;text-indent:0em;border: 1px solid #99CC00; text-align: center;}
-    .type .th{ background:#F1FADE; font-weight:bold; border-top:1px }
+    .type .th{ background:#F1FADE; font-weight:bold; border-top:1px;}
 </style>
 <div class="col-md-offset-2 col-md-8 col-lg-offset-0 col-lg-12">
     <h1>所有属性</h1>
